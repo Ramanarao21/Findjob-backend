400a80a2d6b310b71d46c458459ad70b1fc6d831
Ramanarao21 <ramanaraoVK18@gmail.com>
2026-07-19 15:30:17 +0530

    feat: initialize express application with cors, static file handling, and modular route definitions

--- src/app.js
+++ src/app.js
@@ -14,7 +14,12 @@ const companyRoutes = require('./routes/companyRoutes');
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://find-job-frontend-theta.vercel.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
